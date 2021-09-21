b78e0e54da8a1d23d3a1a052140be699bdfa2f3a
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-09-21 11:22:56 +0530

    Initial commit

---
+++ src/test/java/com/ganesh/controller/DataControllerTest.java
@@ -0,0 +1,47 @@
+package com.ganesh.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * @created: 21/09/2021 - 11:10 AM
+ * @author: Ganesh
+ */
+
+@SpringBootTest
+class DataControllerTest {
+
+    @Autowired
+    DataController dataController;
+
+    @Test
+    @DisplayName("Testing how healthCheck() method works")
+    void healthCheck() {
+        assertEquals("HEALTH CHECK OK !", dataController.healthCheck());
+    }
+
+    @Test
+    @DisplayName("Testing how version() method works")
+    void version() {
+        assertEquals( "The actual version is 1.0.0", dataController.version());
+    }
+
+    @Test
+    @DisplayName("Testing how nationsLenght() method works")
+    void nationsLenght() {
+        Integer nationsLength = dataController.getRandomNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    @DisplayName("Testing how currenciesLength() method works")
+    void currenciesLength() {
+         Integer currenciesLength = dataController.getRandomCurrencies().size();
+         assertEquals(20, currenciesLength);
+    }
+}
